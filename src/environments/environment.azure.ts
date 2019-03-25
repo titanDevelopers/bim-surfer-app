c520d8e4975a9ec2c4b5f77ee72de8d9e0a0182b
JanoSlivka <js.slivka@gmail.com>
2019-03-25 20:07:50 +0100

    Zobrazenie 3d modelu.

---
+++ src/environments/environment.azure.ts
@@ -0,0 +1,6 @@
+export const environment = {
+  production: true,
+  apiUrl: 'http://kros-titan-bim.westeurope.cloudapp.azure.com:5500/bimserverwar-1.5.125',
+  login: 'kros@kros.sk',
+  password: 'Titan123456789',
+};
